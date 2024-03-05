66d470565a8a731e80934e14c4abcbcf422627aa
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2024-03-05 14:43:28 +0700

    router

--- src/projects/react-monkey-blogging/src/firebase/firebase-config.js
+++ src/projects/react-monkey-blogging/src/firebase/firebase-config.js
@@ -1,6 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCJdqkG3iqx7wp9A3ppzS790-kyqWAnlOo",
   authDomain: "monkey-blogging-d1815.firebaseapp.com",
@@ -9,6 +14,8 @@ const firebaseConfig = {
   messagingSenderId: "1004863159428",
   appId: "1:1004863159428:web:38c09b46f9b903281e16d1",
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
